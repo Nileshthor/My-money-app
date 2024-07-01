02c3e1c4ad25608f2a38c5a0ceda5c9fa0d4d0c0
Nileshthor <nileshthorat-inft@atharvacoe.ac.in>
2024-07-01 19:38:47 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/home'
 import Requests from './Components/Requests';
@@ -10,13 +10,13 @@ import { useState } from 'react';
 function App() {
   const [balance, setBalance] = useState(100000)
   return (
-    <Routes basename={process.env.PUBLIC_URL}>
+    <Routes>
       <Route path='/' element={<Home balance={balance} setBalance={setBalance}/>}/>
       <Route path='login' element={ <Login/>}/>
       <Route path='signin' element={<SignIn/>}/>
       <Route path='history' element={<History/>}/>
       <Route path='requests' element={<Requests balance={balance} setBalance={setBalance}/>}/>
-  </Routes>
+    </Routes>
   );
 }
 
