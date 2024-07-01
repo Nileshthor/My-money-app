30efc23b69031bab64e54f3ee8fdc20ebb31fb5a
Nileshthor <nileshthorat-inft@atharvacoe.ac.in>
2024-07-01 18:00:07 +0530

    Configure routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
