6fe96090292d1d9fe1f9660824130e71d5d7431d
Nileshthor <nileshthorat-inft@atharvacoe.ac.in>
2024-07-01 18:27:53 +0530

    routing error fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/home'
 import Requests from './Components/Requests';
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className="App">
         <h1>My Money App</h1>
-      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home balance={balance} setBalance={setBalance}/>}/>
           <Route path='login' element={ <Login/>}/>
@@ -20,7 +19,6 @@ function App() {
           <Route path='history' element={<History/>}/>
           <Route path='requests' element={<Requests balance={balance} setBalance={setBalance}/>}/>
         </Routes>
-      </BrowserRouter>
     </div>
    
   );
